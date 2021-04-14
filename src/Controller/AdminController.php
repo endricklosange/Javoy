1c9fd16d5f959fd5f49b9fa50083d7b69c3954b9
Valentin <valentin.lay98@gmail.com>
2021-04-14 11:21:37 +0200

    Update index admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,9 +1,9 @@
 <?php
+
 namespace App\Controller;
 
 class AdminController extends AbstractController
 {
-    
     public function index()
     {
         return $this->twig->render('Admin/index.html.twig');
