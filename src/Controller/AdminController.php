2ba17f1a29c8b87582441fcebe9e5fd35ca5a9c7
Valentin <valentin.lay98@gmail.com>
2021-04-14 16:40:15 +0200

    Insert loop in product.html

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,5 +7,10 @@ class AdminController extends AbstractController
     public function index()
     {
         return $this->twig->render('Admin/index.html.twig');
+    }    
+    
+    public function add()
+    {
+        return $this->twig->render('Admin/add-products.html.twig');
     }
 }
