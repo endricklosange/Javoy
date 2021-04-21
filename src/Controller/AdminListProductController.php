25e652ef7591a730981dd276959a5af3b2ea325b
endricklosange <endricklosange@gmail.com>
2021-04-21 20:33:37 +0200

    create_listproduct

---
+++ src/Controller/AdminListProductController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+use App\Model\ListProductManager;
+
+class AdminListProductController extends AbstractController
+{
+    public function index(): string
+    {
+        $listProducts = new ListProductManager();
+        $products = $listProducts->selectAll();
+
+        return $this->twig->render('Admin/_listProduct.html.twig', ['products' => $products]);
+    }
+}
