b2fbddb37e625344465d7b46df7bd85b8b59aa94
RedPandore <tennessee-houry_student2021@wilder.school>
2021-04-21 16:46:28 +0200

    Creation page edit

--- src/Model/ProductManager.php
+++ src/Model/ProductManager.php
@@ -26,4 +26,34 @@ class ProductManager extends AbstractManager
 
         return $statement->execute();
     }
+
+    /**
+     * Update item in database
+     */
+    public function update(array $products): void
+    {
+        $statement = $this->pdo->prepare(
+            "UPDATE " . self::TABLE .
+                " SET
+                    `name` = :name,
+                    `price` = :price, 
+                    `description` = :description,
+                    `year` = :year,
+                    `image` = :image,
+                    `created_at` = :created_at,
+                    `category` = :category
+                WHERE id=:id "
+        );
+
+        $statement->bindValue('name', $products['name'], \PDO::PARAM_STR);
+        $statement->bindValue('price', $products['price'], \PDO::PARAM_INT);
+        $statement->bindValue('description', $products['description'], \PDO::PARAM_STR);
+        $statement->bindValue('year', $products['year'], \PDO::PARAM_INT);
+        $statement->bindValue('image', $products['image'], \PDO::PARAM_STR);
+        $statement->bindValue('created_at', $products['created_at'], \PDO::PARAM_STR);
+        $statement->bindValue('category', $products['category'], \PDO::PARAM_STR);
+        $statement->bindValue('id', $products['id'], \PDO::PARAM_INT);
+
+        $statement->execute();
+    }
 }
