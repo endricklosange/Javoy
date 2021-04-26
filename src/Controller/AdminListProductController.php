f323af2c9e1a0cd9fa1c7915fe4c6808eab72cf7
endricklosange <endricklosange@gmail.com>
2021-04-26 11:12:53 +0200

    change underscore

--- src/Controller/AdminListProductController.php
+++ src/Controller/AdminListProductController.php
@@ -11,6 +11,6 @@ class AdminListProductController extends AbstractController
         $listProducts = new ListProductManager();
         $products = $listProducts->selectAll();
 
-        return $this->twig->render('Admin/_listProduct.html.twig', ['products' => $products]);
+        return $this->twig->render('Admin/listProduct.html.twig', ['products' => $products]);
     }
 }
