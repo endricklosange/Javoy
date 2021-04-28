2730510247ba022eb2fdc45ddaf78ddced061cdc
Séverine <severine.coute@laposte.net>
2021-04-28 12:07:31 +0200

    affichage horaires et plan OSM

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,11 +1,10 @@
 <?php
 
-
 namespace App\Controller;
 
 class ContactController extends AbstractController
 {
-   
+
     public function index()
     {
         return $this->twig->render('contact/index.html.twig');
