b7b7a562bc5929214a89c5541cea486ad5304006
endricklosange <endricklosange@gmail.com>
2021-04-28 09:55:11 +0200

    fix white space

--- src/Model/ActualityManager.php
+++ src/Model/ActualityManager.php
@@ -5,7 +5,6 @@ namespace App\Model;
 class ActualityManager extends AbstractManager
 {
     public const TABLE = 'actuality';
-  
     public function insert(array $actuality): void
     {
         $query = "INSERT INTO " . self::TABLE . " (`name`, `description`, `image`)
