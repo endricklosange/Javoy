d43b8c1feba3bd44519c24313314b9e6591e7b55
endricklosange <endricklosange@gmail.com>
2021-04-28 16:27:05 +0200

    fix spelling

--- src/Controller/AdminActualityController.php
+++ src/Controller/AdminActualityController.php
@@ -6,8 +6,8 @@ use App\Model\ActualityManager;
 
 class AdminActualityController extends AbstractController
 {
-    private const NEWS_MAX_LENGHT = 80;
-    private const DESCRIPTION_MAX_LENGHT = 500;
+    private const NEWS_MAX_LENGTH = 80;
+    private const DESCRIPTION_MAX_LENGTH = 500;
 
     public function add(): string
     {
@@ -20,14 +20,14 @@ class AdminActualityController extends AbstractController
             if (empty($actuality['name'])) {
                 $errors[] = 'Le nom est obligatoire';
             }
-            if (strlen($actuality['name']) > self::NEWS_MAX_LENGHT) {
-                $errors[] = 'Le nom doit contenir moin de ' . self::NEWS_MAX_LENGHT . 'caractère';
+            if (strlen($actuality['name']) > self::NEWS_MAX_LENGTH) {
+                $errors[] = 'Le nom doit contenir moins de ' . self::NEWS_MAX_LENGTH . 'caractères';
             }
             if (empty($actuality['description'])) {
                 $errors[] = 'La description est obligatoire';
             }
-            if (strlen($actuality['description']) > self::DESCRIPTION_MAX_LENGHT) {
-                $errors[] = 'Le nom doit contenir moin de ' . self::DESCRIPTION_MAX_LENGHT . 'caractère';
+            if (strlen($actuality['description']) > self::DESCRIPTION_MAX_LENGTH) {
+                $errors[] = 'Le nom doit contenir moins de ' . self::DESCRIPTION_MAX_LENGTH . 'caractères';
             }
             if (empty($actuality['image'])) {
                 $errors[] = 'L\'image est obligatoire';
--- src/Model/ActualityManager.php
+++ src/Model/ActualityManager.php
@@ -7,12 +7,14 @@ class ActualityManager extends AbstractManager
     public const TABLE = 'actuality';
     public function insert(array $actuality): void
     {
-        $query = "INSERT INTO " . self::TABLE . " (`name`, `description`, `image`)
-        VALUES (:name, :description, :image)";
+        $query = "INSERT INTO " . self::TABLE . " (`name`, `description`, `article`,`image`,`created_at`)
+        VALUES (:name, :description, :article, :image, :created_at)";
         $statement = $this->pdo->prepare($query);
         $statement->bindValue('name', $actuality['name'], \PDO::PARAM_STR);
         $statement->bindValue('description', $actuality['description'], \PDO::PARAM_STR);
+        $statement->bindValue('article', $actuality['article'], \PDO::PARAM_STR);
         $statement->bindValue('image', $actuality['image'], \PDO::PARAM_STR);
+        $statement->bindValue('created_at', $actuality['created_at'], \PDO::PARAM_STR);
         $statement->execute();
     }
 }
