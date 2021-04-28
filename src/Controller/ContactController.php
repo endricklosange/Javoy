01ad9ca296be70ad7b693632efae5416fc477d11
Valentin <valentin.lay98@gmail.com>
2021-04-28 12:16:06 +0200

    Include de _formContact dans /Contact/index

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,4 +5,59 @@ namespace App\Controller;
 class ContactController extends AbstractController
 {
 
+    public function index()
+    {
+        $errors = $this->send();
+        return $this->twig->render('Contact/index.html.twig', ["errors" => $errors]);
+    }
+
+    private const NAME_MAX_LENGHT = 80;
+    private const MESSAGE_MAX_LENGHT = 2000;
+
+    public function validate($data)
+    {
+        $errors = [];
+
+        if (empty($data['lastname'])) {
+            $errors[] = 'Le nom est obligatoire';
+        } elseif ($data['lastname'] > self::NAME_MAX_LENGHT) {
+            $errors = 'Le nom doit faire moins de' . self::NAME_MAX_LENGHT . ' caractères';
+        }
+
+        if (empty($data['firstname'])) {
+            $errors[] = 'Le prénom est obligatoire';
+        } elseif ($data['firstname'] > self::NAME_MAX_LENGHT) {
+            $errors = 'Le prénom doit faire moins de' . self::NAME_MAX_LENGHT . ' caractères';
+        }
+
+        if (empty($data['email'])) {
+            $errors[] = 'L\'email est obligatoire';
+        }
+
+        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors[] = 'Mauvais format d\'email';
+        }
+
+        if (empty($data['message'])) {
+            $errors[] = 'Un message est obligatoire';
+        } elseif ($data['message'] > self::MESSAGE_MAX_LENGHT) {
+            $errors = 'Le message doit faire moin de ' . self::MESSAGE_MAX_LENGHT . ' caractères';
+        }
+        return $errors;
+    }
+
+    private function send()
+    {
+        $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = array_map('trim', $_POST);
+            $errors = $this->validate($data);
+
+            if (empty($errors)) {
+                //send in mail
+                header('location: index.html.twig');
+            }
+        }
+        return $errors;
+    }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,58 +20,8 @@ class HomeController extends AbstractController
      * @throws \Twig\Error\SyntaxError
      */
 
-    private const NAME_MAX_LENGHT = 80;
-    private const MESSAGE_MAX_LENGHT = 2000;
-
-    public function validate($data)
-    {
-        $errors = [];
-
-        if (empty($data['lastname'])) {
-            $errors[] = 'Le nom est obligatoire';
-        } elseif ($data['lastname'] > self::NAME_MAX_LENGHT) {
-            $errors = 'Le nom doit faire moins de' . self::NAME_MAX_LENGHT . ' caractères';
-        }
-
-        if (empty($data['firstname'])) {
-            $errors[] = 'Le prénom est obligatoire';
-        } elseif ($data['firstname'] > self::NAME_MAX_LENGHT) {
-            $errors = 'Le prénom doit faire moins de' . self::NAME_MAX_LENGHT . ' caractères';
-        }
-
-        if (empty($data['email'])) {
-            $errors[] = 'L\'email est obligatoire';
-        }
-
-        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
-            $errors[] = 'Mauvais format d\'email';
-        }
-
-        if (empty($data['message'])) {
-            $errors[] = 'Un message est obligatoire';
-        } elseif ($data['message'] > self::MESSAGE_MAX_LENGHT) {
-            $errors = 'Le message doit faire moin de ' . self::MESSAGE_MAX_LENGHT . ' caractères';
-        }
-        return $errors;
-    }
-
-    private function send()
-    {
-        $errors = [];
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $data = array_map('trim', $_POST);
-            $errors = $this->validate($data);
-
-            if (empty($errors)) {
-                //send in mail
-                header('location: index.html.twig');
-            }
-        }
-        return $errors;
-    }
     public function index()
     {
-        $errors = $this->send();
-        return $this->twig->render('Home/index.html.twig', ["errors" => $errors]);
+        return $this->twig->render('Home/index.html.twig');
     }
 }
