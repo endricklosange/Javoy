d1d7b75daaa545c7c9ec8f5b54222697c3e38020
Serdar45000 <serdar45000@gmail.com>
2021-04-30 13:24:55 +0200

    Affichage des détails de la commande et modification du status des commandes

--- src/Controller/AdminOrderController.php
+++ src/Controller/AdminOrderController.php
@@ -11,13 +11,45 @@ class AdminOrderController extends AbstractController
     {
         $statusManager = new StatusManager();
         $statusLists = $statusManager->selectAll();
-
         $status = new AdminOrderManager();
         $orderStatus = $status->selectAllOrderStatus();
 
         return $this->twig->render('Admin/listOrder.html.twig', [
             'statusLists' => $statusLists,
             'orderStatus' => $orderStatus,
+        ]);
+    }
+
+    public function show(int $orderStatus): string
+    {
+        $statusManager = new AdminOrderManager();
+        $orderStatus = $statusManager->selectByIdOrder($orderStatus);
+
+        return $this->twig->render('Admin/showOrder.html.twig', ['orderStatus' => $orderStatus]);
+    }
+
+    public function edit(int $orderStatus): string
+    {
+        $statusManager = new StatusManager();
+        $statusLists = $statusManager->selectAll();
+        $statusOrder = new AdminOrderManager();
+        $orderStatus = $statusOrder->selectByIdOrder($orderStatus);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $statusList = array_map('trim', $_POST);
+            
+            // TODO validations (length, format...)
+            
+            // if validation is ok, update and redirection
+            $orderStatus['status_id'] = $statusList['status'];
+            $statusOrder->update($orderStatus);
+            header('Location: /AdminOrder/index');
+        }
+
+        return $this->twig->render('Admin/editOrder.html.twig', [
+            'orderStatus' => $orderStatus,
+            'statusLists' => $statusLists,
         ]);
     }
 }
--- src/Model/AdminOrderManager.php
+++ src/Model/AdminOrderManager.php
@@ -5,7 +5,6 @@ namespace App\Model;
 class AdminOrderManager extends AbstractManager
 {
     public const TABLE = '`order`';
-    public const TABLESTATUS = 'status';
 
     public function selectAllOrderStatus()
     {
@@ -13,11 +12,39 @@ class AdminOrderManager extends AbstractManager
         .  static::TABLE  .  '.firstname,'  .  static::TABLE  .  '.lastname,'
         .  static::TABLE  .  '.email,'  .  static::TABLE  .  '.address,'
         .  static::TABLE  .  '.zipcode,'  .  static::TABLE  .  '.city,'
-        .  static::TABLE  .  '.detail,' . static::TABLESTATUS  .  '.name FROM '
-        .  static::TABLE  .  ' JOIN ' . static::TABLESTATUS  . ' ON '
-        .  static::TABLESTATUS  .  '.id = '  . static::TABLE  .  '.status_id WHERE '
-        .  static::TABLESTATUS .  '.id='  .  static::TABLE  .  '.status_id';
+        .  static::TABLE  .  '.detail,' . StatusManager::TABLE  .  '.name FROM '
+        .  static::TABLE  .  ' JOIN ' . StatusManager::TABLE  . ' ON '
+        .  StatusManager::TABLE  .  '.id = '  . static::TABLE  .  '.status_id WHERE '
+        .  StatusManager::TABLE .  '.id='  .  static::TABLE  .  '.status_id';
 
         return $this->pdo->query($query)->fetchAll();
+    }
+
+    public function selectByIdOrder(int $id)
+    {
+        // prepared request
+        
+        $statement = $this->pdo->prepare('SELECT'  .  static::TABLE . '.id, '  .  static::TABLE . '.title,'
+        .  static::TABLE  .  '.firstname,'  .  static::TABLE  .  '.lastname,'
+        .  static::TABLE  .  '.email,'  .  static::TABLE  .  '.address,'
+        .  static::TABLE  .  '.zipcode,'  .  static::TABLE  .  '.city,'.  static::TABLE  .  '.status_id,'
+        .  static::TABLE  .  '.country,' 
+        .  static::TABLE  .  '.detail,' . StatusManager::TABLE  .  '.name FROM '
+        .  static::TABLE  .  ' JOIN ' . StatusManager::TABLE  . ' ON '
+        .  StatusManager::TABLE  .  '.id = '  . static::TABLE  .  '.status_id WHERE '  . static::TABLE  .  '.id=:id');
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetch();
+    }
+
+    public function update(array $orderStatus): bool
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET status_id = :status_id WHERE id = :id");
+
+        $statement->bindValue('id', $orderStatus['id'], \PDO::PARAM_INT);
+        $statement->bindValue('status_id', $orderStatus['status_id'], \PDO::PARAM_INT);
+
+        return $statement->execute();
     }
 }
