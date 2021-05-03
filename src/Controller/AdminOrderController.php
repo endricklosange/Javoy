f9695d649a8d3f226a4b931665c170d141f4f5c3
Serdar45000 <serdar45000@gmail.com>
2021-05-03 15:59:46 +0200

    FIX correction

---
+++ src/Controller/AdminOrderController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\AdminOrderManager;
+use App\Model\StatusManager;
+
+class AdminOrderController extends AbstractController
+{
+
+    public function index(): string
+    {
+        $status = new AdminOrderManager();
+        $orderStatus = $status->selectAllOrderStatus();
+
+        return $this->twig->render('Admin/listOrder.html.twig', [
+            'orderStatus' => $orderStatus,
+        ]);
+    }
+
+    public function show(int $orderStatus): string
+    {
+        $statusManager = new StatusManager();
+        $statusLists = $statusManager->selectAll();
+        $statusOrder = new AdminOrderManager();
+        $orderStatus = $statusOrder->selectByIdOrder($orderStatus);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $statusList = array_map('trim', $_POST);
+            $orderStatus['status_id'] = $statusList['status'];
+            $statusOrder->update($orderStatus);
+            header('Location: /AdminOrder/index');
+        }
+
+        return $this->twig->render('Admin/showOrder.html.twig', [
+            'orderStatus' => $orderStatus,
+            'statusLists' => $statusLists,
+        ]);
+    }
+}
