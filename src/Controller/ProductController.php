9cc13ce5264bfe1c68fabc2d8c66cada0c9741a7
Séverine <severine.coute@laposte.net>
2021-05-03 10:10:21 +0200

    fix erreur

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -8,8 +8,8 @@ class ProductController extends AbstractController
 {
     public function index(): string
     {
-        $AllProducts = new ProductManager();
-        $products = $AllProducts->selectAll();
+        $allProducts = new ProductManager();
+        $products = $allProducts->selectAll();
 
         return $this->twig->render('Product/index.html.twig', ['products' => $products]);
     }
