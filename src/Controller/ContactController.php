452311973cc8638564cc174bc49f5e5b4833bfb2
Valentin <valentin.lay98@gmail.com>
2021-05-05 19:30:08 +0200

    Update location

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,41 +8,35 @@ class ContactController extends AbstractController
     private const NAME_MAX_LENGTH = 80;
     private const MESSAGE_MAX_LENGTH = 2000;
 
-    public function index()
-    {
-        $errors = $this->send();
-        return $this->twig->render('Contact/index.html.twig', ["errors" => $errors]);
-    }
-
-    public function validate($data)
+    public function validate()
     {
         $errors = [];
+        $data = [];
 
         if (empty($data['lastname'])) {
             $errors[] = 'Le nom est obligatoire';
         } elseif ($data['lastname'] > self::NAME_MAX_LENGTH) {
-            $errors = 'Le nom doit faire moins de' . self::NAME_MAX_LENGTH . ' caractères';
+            $errors[] = 'Le nom doit faire moins de' . self::NAME_MAX_LENGTH . ' caractères';
         }
 
         if (empty($data['firstname'])) {
             $errors[] = 'Le prénom est obligatoire';
         } elseif ($data['firstname'] > self::NAME_MAX_LENGTH) {
-            $errors = 'Le prénom doit faire moins de' . self::NAME_MAX_LENGTH . ' caractères';
+            $errors[] = 'Le prénom doit faire moins de' . self::NAME_MAX_LENGTH . ' caractères';
         }
 
         if (empty($data['email'])) {
             $errors[] = 'L\'email est obligatoire';
-        }
-
-        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+        } elseif (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
             $errors[] = 'Mauvais format d\'email';
         }
 
         if (empty($data['message'])) {
             $errors[] = 'Un message est obligatoire';
         } elseif ($data['message'] > self::MESSAGE_MAX_LENGTH) {
-            $errors = 'Le message doit faire moins de ' . self::MESSAGE_MAX_LENGTH . ' caractères';
+            $errors[] = 'Le message doit faire moins de ' . self::MESSAGE_MAX_LENGTH . ' caractères';
         }
+        var_dump($data);
         return $errors;
     }
 
@@ -50,14 +44,22 @@ class ContactController extends AbstractController
     {
         $errors = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $data = array_map('trim', $_POST);
-            $errors = $this->validate($data);
+            $errors = $this->validate();
 
             if (empty($errors)) {
                 //send in mail
-                header('location: index.html.twig');
+                header('location: /Contact/index');
             }
         }
         return $errors;
+    }
+
+    public function index()
+    {
+        $errors = $this->send();
+        var_dump($errors);
+        return $this->twig->render('Contact/index.html.twig', [
+            "errors" => $errors
+        ]);
     }
 }
