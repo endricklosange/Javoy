f349dc19073cde89faf93b109a777c41d1275f36
Séverine <severine.coute@laposte.net>
2021-05-06 10:18:24 +0200

    suppression 2 marqueurs du plan

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1,9 +1,8 @@
         // Open Street Map
-        const places = {
+        const place = {
                 "Javoy Père & Fils": { "lat":47.83309, "lon":1.81769 },
-                "Château de Chambord": { "lat":47.616, "lon":1.517 },
-                "Orléans": { "lat":47.902, "lon":1.908 }
         };
+        
         //initialisation de la carte 
         const carte = L.map('maCarte').setView([47.83309, 1.81769], 11);
         
@@ -13,14 +12,12 @@
             minzoom: 1,
             maxZoom: 20,
         }).addTo(carte);
-        
-        //on parcourt les différentes villes
-        for(place in places){
-            // création du marqueur et attribution d'un popup
-            const marker = L.marker([places[place].lat, places[place].lon])
-            .addTo(carte);
-            marker.bindPopup("<p><strong>"+place+"</strong></p>");
-        }
+    
+
+        // création du marqueur et attribution d'un popup
+        const marker = L.marker([47.83309, 1.81769]).addTo(carte);
+        marker.bindPopup("<p><strong>Javoy Père & Fils</strong></p>");
+      
   
 
 
